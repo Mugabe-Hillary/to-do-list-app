abbc1f453436b05300d4346619b677219527a307
Mugabe-Hillary <amhillarx@gmail.com>
2025-06-27 22:39:30 +0300

    Fix: Refactored the code: separating concerns in script.js and adding state.

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
+// --- SELECTORS ---
 // Select the DOM elements and store in variables.
 const form = document.querySelector("#task-form");
 const taskInput = document.querySelector("#task-input");
 const taskList = document.querySelector("#task-list");
 
+// --- STATE ---
+// Global tasks array variable for data persistance.
+let tasks = [];
+
+// --- FUNCTIONS ---
+// Dynamically update the task list( erase and add new tasks)
+function renderTasks() {
+  taskList.innerHTML = "";
+
+  tasks.forEach(function(task) {
+    const li = document.createElement("li");
+    li.innerText = task.text;
+    taskList.appendChild(li);
+  });
+}
+
+// Add task to the tasks array and then call renderTasks()
+function addTask(taskText) {
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  renderTasks();
+}
+
+// --- EVENT LISTENERS ---
 form.addEventListener("submit", function(event) {
-  //Prevent the form's default behavior
   event.preventDefault();
-
-  //Get the text from the input field and log it
   const taskText = taskInput.value.trim();
-  if (taskText === "") {
-    alert("Please enter a valid task!");
+
+  if (taskText !== "") {
+    addTask(taskText);
+    taskInput.value = "";
     taskInput.focus();
-    return;
+  } else {
+    alert("Please enter a valid task!");
+    taskInput.value = "";
+    taskInput.focus();
   }
-
-  // Create a new 'li' element, add the input text and append it to the task list.
-  const newTask = document.createElement("li");
-  newTask.innerText = taskText;
-  taskList.appendChild(newTask);
-
-  // Clear the input field and reset cursor
-  taskInput.value = "";
-  taskInput.focus();
 });
